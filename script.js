6d897b326b211068ccb1b7d311009eca2026b9b0
Timóteo Mendonça Neto <timoteo.m@uni9.edu.br>
2021-01-07 11:16:36 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function clickei(){
+    var inicio = Number(document.getElementById('inicio').value)
+    var fim = Number(document.getElementById('fim').value)
+    var passo = Number(document.getElementById('passo').value)
+    //var res = Number(document.getElementById('res'))
+    for(var i = inicio; inicio > fim; inicio++  )
+    {res.innerHTML = (inicio)}
+    //res.innerHTML = `Essa eh a resposta ${resposta}`
+}
