9d25f14753ff4a47efa87a3e061f0d58af6722f0
Timóteo Mendonça Neto <timoteo.m@uni9.edu.br>
2021-01-08 07:42:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 function clickei(){
-    var inicio = Number(document.getElementById('inicio').value)
-    var fim = Number(document.getElementById('fim').value)
-    var passo = Number(document.getElementById('passo').value)
-    //var res = Number(document.getElementById('res'))
-    for(var i = inicio; inicio > fim; inicio++  )
-    {res.innerHTML = (inicio)}
-    //res.innerHTML = `Essa eh a resposta ${resposta}`
+    var inic = document.getElementById('inicio')
+    var fin = document.getElementById('fim')
+    var pas = document.getElementById('passo')
+
+    if( inic.value.length ==0 || fin.value.length == 0 || pas.value.length == 0){
+        alert("Vc precisa digitar para funcionar!!")
+    }
+    else{
+        var inicio = Number(inic.value)
+        var fim = Number(fin.value)
+        var passo = Number(pas.value)
+        if(passo == 0){
+            alert('No minimo 1')
+            passo = 1
+            
+        }
+    }
+    if(inicio < fim){
+        do{
+            inicio += passo
+            res.innerHTML += `${inicio} <br>`
+        }while(inicio <= fim)
+    }else{
+        do{
+            inicio -= passo
+            res.innerHTML += `${inicio} <br>`
+        }while(inicio >= fim)
+    }
 }
