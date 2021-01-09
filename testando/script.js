b728a0ef033d105bc5f561d143a7d2bc77893b5d
Timóteo Mendonça Neto <timoteo.m@uni9.edu.br>
2021-01-09 20:57:15 -0300

    Update script.js

--- testando/script.js
+++ testando/script.js
@@ -3,23 +3,29 @@
         var sell = document.getElementById("sel1")
 
         if (numero.value.length == 0){
-            alert('Digite algo')
+            alert('Você precisa digitar um número')
             
+        }else if(numero.value < 1 ){
+            alert("O número está abaixo que o solcitado!")
+        }else if(numero.value > 100){
+            alert("O número está acima que o solcitado!")
         }else{
             var num1 = Number(numero.value)
             var sel = Number(sell.value)
             let vet = []
-            var c = 0
             vet.push(num1)
             
             let cont = vet.length
 
-            //sell.innerHTML =''
-            while(c <= cont){
-                let item = document.createElement('option')
-                item.text = `${vet[c]} e ${cont}`
-                //sel1.appendChild(item)
-                c++
+            let achou = vet.indexOf(num1)
+            if (achou != 0){
+                alert("numero repetido")
             }
+
+                for (let i = 0; i < cont; i++  ){
+                    let item = document.createElement('option')
+                    item.text = `${vet[i]}`
+                    sel1.appendChild(item)
+                }
         }
     }
