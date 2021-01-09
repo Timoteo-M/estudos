a5093a3c322a58b2247dd6f353bf031b52d1b40c
Timóteo Mendonça Neto <timoteo.m@uni9.edu.br>
2021-01-09 09:08:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-function clickei(){
+function clickey(){
     var inic = document.getElementById('inicio')
     var fin = document.getElementById('fim')
     var pas = document.getElementById('passo')
 
-    if( inic.value.length ==0 || fin.value.length == 0 || pas.value.length == 0){
-        alert("Vc precisa digitar para funcionar!!")
-    }
-    else{
+    if (inic.value.length == 0 || fin.value.length == 0 || pas.value.length == 0){
+        alert('Precisa preencher todos os dados!!')
+    }else{
         var inicio = Number(inic.value)
         var fim = Number(fin.value)
         var passo = Number(pas.value)
-        if(passo == 0){
-            alert('No minimo 1')
-            passo = 1
-            
+
+        if(inicio < fim){
+            for (var i = inicio; i <= fim; i += passo){
+                res.innerHTML += (i + "<br>")
+            }
+        }else{
+            for (var i = inicio; i >= fim; i -= passo){
+                res.innerHTML += (i + "<br>")
+            }
         }
     }
-    if(inicio < fim){
-        do{
-            inicio += passo
-            res.innerHTML += `${inicio} <br>`
-        }while(inicio <= fim)
-    }else{
-        do{
-            inicio -= passo
-            res.innerHTML += `${inicio} <br>`
-        }while(inicio >= fim)
-    }
+
 }
