b6991f9749bc14beff24a6e4c0cf4a94b6c4a50c
Timóteo Mendonça Neto <timoteo.m@uni9.edu.br>
2021-01-10 12:07:23 -0300

    Create script.js

---
+++ Contador/script.js
@@ -0,0 +1,30 @@
+function clickey(){
+    var inic = document.getElementById('inicio')
+    var fin = document.getElementById('fim')
+    var pas = document.getElementById('passo')
+
+    if (inic.value.length == 0 || fin.value.length == 0 || pas.value.length == 0){
+        alert('Precisa preencher todos os dados!!')
+    }else{
+        var inicio = Number(inic.value)
+        var fim = Number(fin.value)
+        var passo = Number(pas.value)
+
+        if(passo == 0){
+            alert('Valor incorreto!!<br> SUbstituido por 1')
+            passo = 1
+        }
+        res.innerHTML = ''
+
+        if(inicio < fim){
+            for (var i = inicio; i <= fim; i += passo){
+                res.innerHTML += (i + "<br>")
+            }
+        }else{
+            for (var i = inicio; i >= fim; i -= passo){
+                res.innerHTML += (i + "<br>")
+            }
+        }
+    }
+
+}
